ac4333862afc6a1801bdfc097f76d0ceb27f63c3
runcloud <runcloud@sunth.onmicrosoft.com>
2022-05-21 11:10:50 +0800

    edit someting

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,14 @@
 </script>
 
 <main>
+	<div>
+		<label> Name: <input type="text" bind:value="{name}" /> </label>
+	</div>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
 </main>
 
 <style>
